ba8b3533f40aff2a7036ec7e1430b75d6b0ab375
Kate Audrey Ongcoy <if20b253@technikum-wien.at>
2022-12-04 23:20:30 +0100

    final push

---
+++ README.txt
@@ -0,0 +1,12 @@
+Team members:
+Alice Mihalca if20b251,
+Kate Audrey Ongcoy if20b253
+
+GitHub Link:
+https://github.com/kaetey/WordCounter.git
+
+How to run:
+1. open second WordCounter director
+	WordCounter> cd WordCounter
+2. enter following line (replace path)
+	dotnet run path txt
--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -1,2 +1,53 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string mainPath = Environment.GetCommandLineArgs()[1];
+string fileExtension = Environment.GetCommandLineArgs()[2];
+
+string[] filePaths = System.IO.Directory.GetFiles(mainPath, $"*.{fileExtension}", SearchOption.AllDirectories);
+
+//Console.WriteLine(dictToString(dictionary));
+
+var getFileContent = (string file) =>
+{
+    return File.ReadAllText(file).ToLower();
+};
+
+string mergeFileContent(string[] filePaths)
+{
+    if (filePaths.Length == 0)
+    {
+        return "";
+    }
+    return getFileContent(filePaths[0]) + " " + mergeFileContent(filePaths.Skip(1).ToArray());
+}
+
+var constructDict = (string[] words) =>
+{
+    Dictionary<string, int> dict = new Dictionary<string, int>();
+    foreach (string word in words)
+    {
+        if (!dict.ContainsKey(word))
+            dict.Add(word, 1);
+        else
+            ++dict[word];
+    }
+    return dict;
+};
+
+var dictToString = (Dictionary<string, int> dict) => {
+    string sol = "";
+    foreach (var pair in dict)
+    {
+        string key = pair.Key;
+        int val = pair.Value;
+
+        sol += key + " " + val + "\n";
+    }
+    return sol;
+};
+
+string[] words = mergeFileContent(filePaths).Split(' ', ',', '*', '.', '-', '!', '?', '(', ')', '[', ']', '"', ':', ';', '\r', '\n');
+
+Dictionary<string, int> dictionary = constructDict(words);
+
+await File.WriteAllTextAsync("solution.txt", dictToString(dictionary));
